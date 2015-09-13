a7543d8a4edc26fa6d41884ab1556678155037ea
cstavitsky <corridorr@gmail.com>
2015-09-13 15:57:11 -0500

    update host to be heroku app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  host = "localhost:3000"  
+  host = "http://translucent-app.herokuapp.com"  
 
 require "smtp_tls"
 
